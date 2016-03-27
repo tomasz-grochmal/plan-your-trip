c6465e671bc509a37c42a0d42d19652e9849783e
tomasz-grochmal <techtomgroch@gmail.com>
2016-03-27 18:58:15 +0200

    Init commit based on angular2 seed

---
+++ src/app/components/app.component.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
+import {NavbarComponent} from './navbar.component';
+import {ToolbarComponent} from './toolbar.component';
+import {HomeComponent} from '../../home/components/home.component';
+import {GraphComponent} from '../../graph/components/graph.component';
+import {AboutComponent} from '../../about/components/about.component';
+import {NameListService} from '../../shared/services/name-list.service';
+
+@Component({
+  selector: 'sd-app',
+  viewProviders: [NameListService],
+  moduleId: module.id,
+  templateUrl: './app.component.html',
+  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
+})
+@RouteConfig([
+  { path: '/',      name: 'Home',  component: HomeComponent  },
+  { path: '/graph', name: 'Graph',  component: GraphComponent  },
+  { path: '/about', name: 'About', component: AboutComponent }
+])
+export class AppComponent {}
---
+++ src/graph/components/graph.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'sd-graph',
+  moduleId: module.id,
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.css']
+})
+export class GraphComponent {}
